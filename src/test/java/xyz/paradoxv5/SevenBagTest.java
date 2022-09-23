bc98d8355b35a622aa45dbaa9b2f0d255c2b42f7
ParadoxV5 <paradox.ver5@gmail.com>
2022-09-22 19:00:17 -0600

    create `SevenBagTest#test_get_useGivenRandom`

--- src/test/java/xyz/paradoxv5/SevenBagTest.java
+++ src/test/java/xyz/paradoxv5/SevenBagTest.java
@@ -1,17 +1,30 @@
 package xyz.paradoxv5;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.*;
 import java.util.TreeSet;
+import java.util.Random;
 
-public final class SevenBagTest {
+public class SevenBagTest {
   
-  @RepeatedTest(3) public void test_get_everyPiece() {
+  @RepeatedTest(2) public void test_get_everyPiece() {
     SevenBag sevenBag = new SevenBag();
     TreeSet<SevenBag.Tetromino> testSet = new TreeSet<>();
     for(byte c = 0; c < 7; ++c)
       testSet.add(sevenBag.get());
-    assertEquals(SevenBag.Tetromino.SET, testSet);
+    Assertions.assertEquals(SevenBag.Tetromino.SET, testSet);
+  }
+  
+  @RepeatedTest(2) public void test_get_useGivenRandom() {
+    Random random = new Random();
+    long seed = random.nextLong();
+    SevenBag sevenBag = new SevenBag(random);
+    SevenBag.Tetromino[][] array = new SevenBag.Tetromino[2][7];
+    for(int j = 0; j < 2; j++) {
+      random.setSeed(seed);
+      for(int i = 0; i < 7; i++)
+        array[j][i] = sevenBag.get();
+    }
+    Assertions.assertArrayEquals(array[0], array[1]);
   }
   
 }
